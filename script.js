a845ed441ad7a0c12a548665efd097cce3184ebd
Animesh Kumar <animesh99dev@gmail.com>
2021-09-06 13:15:49 +0530

    Finished the desktop verson

--- script.js
+++ script.js
@@ -2,28 +2,59 @@
 
 // Variables
 const newGame = document.querySelector("#new-game");
+
 const roll = document.querySelector("#roll");
+
 const pass = document.querySelector("#pass");
+
 const howTo = document.querySelector("#how-to");
+
 const rename1 = document.querySelector("#rename1");
+
 const rename2 = document.querySelector("#rename2");
+
 const yes = document.querySelector("#yes");
+
 const no = document.querySelector("#no");
+
 const close = document.querySelector("#close");
+
 const identity1 = document.querySelector("#name1");
+
 const identity2 = document.querySelector("#name2");
+
 const card1 = document.querySelector("#card-1");
+
 const card2 = document.querySelector("#card-2");
+
 const theDice = document.querySelector("#the-dice");
+
 const currentScore1 = document.querySelector("#current-score1");
+
 const currentScore2 = document.querySelector("#current-score2");
+
 const totalScore1 = document.querySelector("#total-score1");
+
 const totalScore2 = document.querySelector("#total-score2");
+
 const overlay = document.querySelector("#overlay");
+
 const warning = document.querySelector("#warning");
+
 const inst = document.querySelector("#inst");
+
+const popupRename = document.querySelector("#popup-rename");
+
+const reAssignBtn = document.querySelector("#re-assign-btn");
+
+const nameField = document.querySelector("#name-field");
+
+const mainHeading = document.querySelector("#main-heading");
+
 let activePlayer = 1;
 let diceNumber;
+let renameRequest = 1;
+let range = 50;
 
 // Functions
 function setActivePlayer() {
@@ -37,6 +68,7 @@ function setActivePlayer() {
     card2.classList.add("active");
     currentScore1.textContent = "0";
     activePlayer = 2;
+    winOrLoose();
   } else {
     let current;
     let final;
@@ -47,6 +79,7 @@ function setActivePlayer() {
     card1.classList.add("active");
     currentScore2.textContent = "0";
     activePlayer = 1;
+    winOrLoose();
   }
 }
 
@@ -117,6 +150,16 @@ function instPopupOpen() {
   }
 }
 
+function renamePopupOpen() {
+  if (
+    overlay.classList.contains("hidden") &&
+    popupRename.classList.contains("hidden")
+  ) {
+    popupRename.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+  }
+}
+
 function popupClose() {
   if (!overlay.classList.contains("hidden")) {
     overlay.classList.add("hidden");
@@ -127,4 +170,53 @@ function popupClose() {
   if (!inst.classList.contains("hidden")) {
     inst.classList.add("hidden");
   }
+  if (!popupRename.classList.contains("hidden")) {
+    popupRename.classList.add("hidden");
+  }
+}
+
+function playAgain() {
+  identity1.textContent = "Player 1";
+  identity2.textContent = "Player 2";
+  currentScore1.textContent = "0";
+  currentScore2.textContent = "0";
+  totalScore1.textContent = "0";
+  totalScore2.textContent = "0";
+  document.getElementById("roll").style.pointerEvents = "all";
+  document.getElementById("pass").style.pointerEvents = "all";
+  mainHeading.textContent = `Roll The Dice.`;
+  popupClose();
+}
+
+function currentInput(inp) {
+  renameRequest = inp;
+}
+
+function changeName() {
+  if (renameRequest === 1) {
+    identity1.textContent = nameField.value;
+  } else {
+    identity2.textContent = nameField.value;
+  }
+  nameField.value = "";
+  popupClose();
+}
+
+function winOrLoose() {
+  if (totalScore1.textContent >= range) {
+    mainHeading.textContent = `${identity1.textContent} Won`;
+    document.getElementById("roll").style.pointerEvents = "none";
+    document.getElementById("pass").style.pointerEvents = "none";
+  }
+  if (totalScore2.textContent >= range) {
+    mainHeading.textContent = `${identity2.textContent} Won`;
+    document.getElementById("roll").style.pointerEvents = "none";
+    document.getElementById("pass").style.pointerEvents = "none";
+  }
+}
+
+function changeRange() {
+  range = Number(document.getElementById("range-field").value);
+  document.getElementById("range-field").value = "";
+  popupClose();
 }
